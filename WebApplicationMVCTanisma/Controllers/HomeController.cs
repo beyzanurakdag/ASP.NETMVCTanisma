0a66ea1ae144d57737ac4376f75416ab0be11b9b
103SABAH_BEYZANUR <103SABAH_BEYZANUR@DESKTOP-TUMHS1A>
2022-01-10 14:47:09 +0300

    Add project files.

---
+++ WebApplicationMVCTanisma/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationMVCTanisma.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Anasayfa()
+        {
+            return View();
+        }
+        public ActionResult Deneme()
+        {
+            //ViewBag nedir?
+            ViewBag.Ad = "Betül";
+            //ViewData
+            ViewData["Ad"] = "Ali";
+            //ViewData ile viewbag birbirinin aynısıdır.Sadece yazımları farklı.Ekranda viewbag içinde ali'yi göreceğiz.Çünkü yeniden set edilmiş.
+            ViewData["Soyad"] = "Akşan";
+            //TempData
+            TempData["Adiniz"] = "Betül";
+            return View();
+        }
+    }
+}
---
+++ WebApplicationMVCTanisma/Models/Ogrenci.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationMVCTanisma.Models
+{
+    public class Ogrenci
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+
+        public static List<Ogrenci> OgrenciListesi { get; set; } = new List<Ogrenci>();
+
+
+
+
+    public static List<Ogrenci> OgrencileriGetir()
+    {
+        if (OgrenciListesi.Count == 0)
+        {
+            OgrenciListesi = new List<Ogrenci>()
+            {
+                new Ogrenci(){Id=1, Ad="Betül",Soyad="Akşan",DogumTarihi=new DateTime(1992,11,14) },
+                 new Ogrenci(){Id=2, Ad="Lale",Soyad="Laleoğlu",DogumTarihi=new DateTime(1990,11,14) },
+                  new Ogrenci(){Id=3, Ad="Deniz",Soyad="Denizoğlu",DogumTarihi=new DateTime(1991,11,14) },
+                   new Ogrenci(){Id=4, Ad="Sümbül",Soyad="Sümbüloğlu",DogumTarihi=new DateTime(1980,11,14) }
+            };
+        }
+
+        return OgrenciListesi;
+    }
+
+    }    
+}
